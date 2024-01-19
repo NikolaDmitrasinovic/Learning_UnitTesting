f49da6daaf9d1f5c122536ffbc754e728979530f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-19 14:26:15 +0100

    Asserting on Structural Integrity of Source Code

---
+++ WithFluentAssertions/Loans/Class1.cs
@@ -0,0 +1,15 @@
+﻿//using Loans.Domain;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Loans
+//{
+//    internal class Class1 : ValueObject
+//    {
+//        protected override IEnumerable<object> GetAtomicValues()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
---
+++ WithFluentAssertions/Loans/Domain/Applications/Values/Class2.cs
@@ -0,0 +1,10 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Loans.Domain.Applications.Values
+//{
+//    internal class Class2
+//    {
+//    }
+//}
